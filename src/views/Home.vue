c8c9c1e95c36c5d08ac9166d18a54dbd05975322
Aml Walaed <amlwalaed@gmail.com>
2025-07-07 21:03:08 +0200

    feat: add button component with variants and utility functions
    - Created a new configuration file `components.json` for component settings.
    - Implemented a `Button` component in `src/components/ui/button/Button.vue` with support for variants and sizes.
    - Defined button variants and sizes in `src/components/ui/button/index.ts` using class-variance-authority.
    - Added utility function `cn` in `src/utils/index.ts` for merging Tailwind CSS classes.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,5 +3,6 @@
 <template>
   <main>
     <h1 class="text-brand">amlds sdksodk</h1>
+    <Button>aml walaed</Button>
   </main>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,20 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 import { VitePWA } from 'vite-plugin-pwa'
 import tailwindcss from '@tailwindcss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import path from 'node:path'
+import fs from 'node:fs'
+
+function getUIComponentFolders() {
+  const uiDirectory = path.resolve('./src/components/ui') // Change this to your UI components directory path
+  const folders = fs
+    .readdirSync(uiDirectory, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name)
+  return folders
+}
+const uiComponents = getUIComponentFolders()
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -18,6 +32,46 @@ export default defineConfig({
       devOptions: {
         enabled: true,
       },
+    }),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      include: [/\.[tj]sx?$/],
+      dirs: ['./src/utils', './src/composables'],
+      vueTemplate: true,
+      dts: true,
+    }),
+    Components({
+      dts: './auto-components.d.ts',
+      resolvers: [
+        {
+          resolve: (componentName) => {
+            if (['RouterLink'].includes(componentName)) {
+              return {
+                name: componentName,
+                from: 'vue-router',
+              }
+            }
+          },
+          type: 'component',
+        },
+      ],
+
+      directoryAsNamespace: true,
+      collapseSamePrefixes: true,
+      globalNamespaces: [
+        'src',
+        'views',
+        'Layouts',
+        'layouts',
+        'Partials',
+        'partials',
+        'Components',
+        'components',
+        'ui',
+        'app',
+        ...uiComponents,
+      ],
+      dirs: ['./src/'],
     }),
   ],
   resolve: {
